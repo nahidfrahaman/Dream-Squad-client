c2aa8e5ba8e4db5cb41612a5add7fee2764d7a53
Nahidfrahaman <nahi.frahaman@gmail.com>
2022-08-22 16:31:42 +0600

    budger section js is done

--- app.js
+++ app.js
@@ -1,3 +1,29 @@
+// common function 
+function getInputValueById (elemntId){
+    const inputField= document.getElementById(elemntId);
+    const inputFieldString= inputField.value;
+    const inputNumber= parseInt(inputFieldString);
+    return inputNumber;
+}
+
+
+// add addEventListener to calculate btn 
 document.getElementById('caluclte-expence').addEventListener('click', function(){
+  const costPerPlayer= getInputValueById('per-player-cost-input')
+  const totalExpence= costPerPlayer *5;
+  const totalExpenceElement= document.getElementById('total-expence');
+  totalExpenceElement.innerText= totalExpence;
+  
+//   add event listener to calculate totaal btn 
+    document.getElementById('total-cal-btn').addEventListener('click', function(){
+    const costForManager= getInputValueById('cost-for-manager-field')
+    const costForCoach= getInputValueById('cost-for-coach-field');
     
-})
+    const costToatl = totalExpence + costForCoach + costForManager;
+    const totalCostElement= document.getElementById('total-cost');
+    totalCostElement.innerText= costToatl;
+    })
+  
+  })
+
+  
