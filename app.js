69c898fe7959d5a73c380cb63ee2d2352d30b0ea
Nahidfrahaman <nahi.frahaman@gmail.com>
2022-08-23 18:47:42 +0600

    ready for submit

--- app.js
+++ app.js
@@ -49,24 +49,27 @@ function getInputValueById (elemntId){
     
 }
 
-
+let totalExpence= 0;
 // add addEventListener to calculate btn 
 document.getElementById('caluclte-expence').addEventListener('click', function(){
   const costPerPlayer= getInputValueById('per-player-cost-input')
-  const totalExpence= costPerPlayer *playerArray.length;
+  totalExpence= costPerPlayer *playerArray.length;
   const totalExpenceElement= document.getElementById('total-expence');
   totalExpenceElement.innerText= totalExpence;
+  
+})
   
 //   add event listener to calculate totaal btn 
     document.getElementById('total-cal-btn').addEventListener('click', function(){
     const costForManager= getInputValueById('cost-for-manager-field')
     const costForCoach= getInputValueById('cost-for-coach-field');
     
+    
     const costToatl = totalExpence + costForCoach + costForManager;
     const totalCostElement= document.getElementById('total-cost');
     totalCostElement.innerText= costToatl;
     })
   
-  })
+  
 
   
